01af1a9b306ad17b269b79850ca512120de88a3c
john smith <john@john.john>
2020-07-30 13:21:01 +1000

    This seems more intuitive when written down

---
+++ standard_deviation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import math
+
+example_dataset = [1,2,3,4,7,8,13,20]
+
+number_of_data = len(example_dataset) 
+
+arithmetic_mean = sum(example_dataset)/number_of_data
+print("Arithmetic mean:", arithmetic_mean)
+
+deviations_from_mean = []
+for elem in example_dataset:
+    deviation = elem - arithmetic_mean
+    deviations_from_mean.append(deviation)
+print("Deviations from mean:", deviations_from_mean)
+
+print("Sum of deviations will always be zero:", sum(deviations_from_mean))
+
+deviations_squared = []
+for elem in deviations_from_mean:
+    deviation_squared = (elem - arithmetic_mean)**2
+    deviations_squared.append(deviation_squared)
+sum_of_squares = sum(deviations_squared)
+print("Total sum of squares (TSS) will be more useful:", sum_of_squares)
+
+arithmetic_mean_of_total_sum_of_squares = sum_of_squares/number_of_data
+print("The average deviation_squared:", arithmetic_mean_of_total_sum_of_squares)
+
+print("This is still squared, so to undo that, take the root:", math.sqrt(arithmetic_mean_of_total_sum_of_squares))
+
+print("This is the same as taking the root of the sum of squares, and then finding the average from the root of n:", math.sqrt(sum_of_squares)/math.sqrt(number_of_data))
+print("This simply means, that since all terms were squared, then all terms must have their root found")
