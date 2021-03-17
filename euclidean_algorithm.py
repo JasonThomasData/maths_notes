22b48e53e2151b349dc6763d954ac14c55395f87
john smith <john@john.john>
2021-03-17 14:10:37 +1100

    For finding the common divisors

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -2,15 +2,22 @@
 
 #given the positive integers a and b, find the largest common factor
 
-a = 360
-b = 756
+a = 6
+b = 238
+
+if (a < b):
+    a,b = b,a
+
+def shortcut(a,b):
+    # Given gcd(a,b) -> a = bq + r -> gcd(b,r)
+    # This essentially collapses the cyclical reduction of c = a - b to find the remainder with modulo
+    r = a % b
+    return b,r
+
+a, b = shortcut(a, b)
 
 c = 0
-
 while True:
-    if (a < b):
-        a,b = b,a
-
     if (a == b):
         print("gcd:", a)
         exit(0)
@@ -21,4 +28,3 @@ while True:
     else:
         a = b
         b = c
-
