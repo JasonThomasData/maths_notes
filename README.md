00455fe8071e6de0caeb2dc9429eca953f427f15
john@john.john <john@john.john>
2021-07-28 23:08:05 +1000

    Some stuff about installing extensions

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ This is a warehouse for algorithms/things I learn that are interesting.
 
 I wholly intend to reinvent the wheel, without packages, where possible so that I can learn more.
 
+### Setup
+
+Install vscode
+
+Run: `.install_extensions.sh`
+
---
+++ install_extensions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+code --install-extension ms-toolsai.jupyter
+code --install-extension toasty-technologies.octave
+code --install-extension paulosilva.vsc-octave-debugger
+code --install-extension leafvmaple.octave
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension vscodevim.vim
