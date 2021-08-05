d9bf96385d90e71ec927243be4102ab73f6db0f1
john@john.john <john@john.john>
2021-08-05 21:12:59 +1000

    Don't need these anymore

--- install_extensions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-code --install-extension ms-toolsai.jupyter
-code --install-extension toasty-technologies.octave
-code --install-extension paulosilva.vsc-octave-debugger
-code --install-extension leafvmaple.octave
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension vscodevim.vim
