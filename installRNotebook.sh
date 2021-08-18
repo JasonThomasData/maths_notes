4e731bb85cc410c67508198883f88485bc899b25
john@john.john <john@john.john>
2021-08-18 21:48:54 +1000

    Required for some R packages

--- installRNotebook.sh
+++ installRNotebook.sh
@@ -9,6 +9,7 @@ pip install jupyter
 
 echo "R INSTALLS"
 sudo apt install r-base
+sudo apt install libcurl4-openssl-dev
 echo "r = getOption('repos'); r['CRAN'] = 'https://cran.csiro.au/'; options(repos = r); install.packages('IRkernel')" | R --vanilla
 echo "IRkernel::installspec()" | R --vanilla
 
