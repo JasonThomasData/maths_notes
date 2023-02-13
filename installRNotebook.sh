2e9307513b07e67e40d4eba694c235cad0aea9a5
John Smith <john@smith.com>
2023-02-13 16:00:17 +1100

    Vim Keys

--- installRNotebook.sh
+++ installRNotebook.sh
@@ -16,3 +16,13 @@ sudo apt install libcurl4-openssl-dev
 echo "r = getOption('repos'); r['CRAN'] = 'https://cran.csiro.au/'; options(repos = r); install.packages('IRkernel')" | R --vanilla
 echo "IRkernel::installspec()" | sudo  --vanilla
 
+echo "SETUP VIM KEYS"
+
+# Create required directory in case (optional)
+mkdir -p $(jupyter --data-dir)/nbextensions
+# Clone the repository
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+# Activate the extension
+jupyter nbextension enable vim_binding/vim_binding
+
