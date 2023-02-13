e07441a4577ae3a6f5a4801694b20a26d56cd747
John Smith <john@smith.com>
2023-02-13 14:33:57 +1100

    More thorough installs

--- installRNotebook.sh
+++ installRNotebook.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+sudo apt install octave
+
+sudo apt install virtualenv
 echo "CREATE, ACTIVATE VENV"
 virtualenv venv -p python3
 source venv/bin/activate
 
 echo "PYTHON INSTALLS"
-pip install jupyter
+pip install jupyter octave-kernel
 
 echo "R INSTALLS"
 sudo apt install r-base
 sudo apt install libcurl4-openssl-dev
 echo "r = getOption('repos'); r['CRAN'] = 'https://cran.csiro.au/'; options(repos = r); install.packages('IRkernel')" | R --vanilla
-echo "IRkernel::installspec()" | R --vanilla
+echo "IRkernel::installspec()" | sudo  --vanilla
 
