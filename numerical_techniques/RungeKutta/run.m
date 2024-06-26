a665f5ee8c09bc548aaacc3face218e78ae258af
John Smith <john@workbot>
2024-06-26 22:45:05 +1000

    .

--- numerical_techniques/RungeKutta/run.m
+++ numerical_techniques/RungeKutta/run.m
@@ -1,23 +1,51 @@
 h = 0.1;
 upperBound = 1.2;
 
-init = [1;0];
+format long;
 
+<<<<<<< Updated upstream
 pkg load symbolic;
 syms x  y;
+=======
+%%
+% 
+%   for x = 1:10
+%       disp(x)
+%   end
+% 
+>>>>>>> Stashed changes
 
-u = [1;
-    (2/x)*sqrt(y - log(x)) + 1/x];
+x0 = 1;
+y0 = 0;
 
-u = [1];
-init = [1];
+% Not autonomous
+yRK1 = RK1(@ydash, x0, y0, h, upperBound);
 
-yRK2 = RK2_Autonomous(u, init, h, upperBound);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Now for the autonomous equations
+
+init = [x0; y0];
+
+syms x y;
+f = [1;
+     (2/x)*sqrt(y - log(x)) + 1/x];
+
+RK1_a = RK1_auton(f, init, [x;y], h, upperBound);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Now for the autonomous equation RK4
+
+N = round(abs(upperBound-init(1))/h)+1;
+RK4_a = RK4_auton(f, init, [x;y], h, N);
+[~, yRK4] = RungeKuttaOrder4(@ydash, x0, y0, h, upperBound);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compare to exact
 
 xSpace = x0:h:upperBound;
 yExact = exact(xSpace);
 
-function res = ydash(y)
+function res = ydash(x, y)
     res = (2/x)*sqrt(y - log(x)) + 1/x;
 end
 
